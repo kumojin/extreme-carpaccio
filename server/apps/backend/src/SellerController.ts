9e36bb03c09624c04c06b1bc7f69e2dd131b9642
Xavier Balloy <686305+xballoy@users.noreply.github.com>
2022-11-13 22:53:10 -0500

    refactor: use joi to validate inputs

--- server/apps/backend/src/SellerController.ts
+++ server/apps/backend/src/SellerController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express-serve-static-core';
 import { StatusCodes } from 'http-status-codes';
+import Joi from 'joi';
 import { SellerService } from './services';
 import { isValidUrl } from './utils';
 
@@ -13,30 +14,43 @@ export const listSellers =
     response.status(StatusCodes.OK).send(sellerViews);
   };
 
+const sellersHistoryValidator = Joi.number().required();
 export const sellersHistory =
   (sellerService: SellerService) => (request: Request, response: Response) => {
-    let chunk: number;
-    if (request.query.chunk && typeof request.query.chunk === 'string') {
-      chunk = parseInt(request.query.chunk, 10);
-    } else {
-      chunk = 10;
-    }
+    const { error, value } = sellersHistoryValidator.validate(
+      request.query.chunk
+    );
+    const chunk = !error ? value : 10;
     response.status(StatusCodes.OK).send(sellerService.getCashHistory(chunk));
   };
 
+type RegisterSellerRequest = {
+  name: string;
+  password: string;
+  url: string;
+};
+const registerSellerValidator = Joi.object<RegisterSellerRequest>({
+  name: Joi.string().required(),
+  password: Joi.string().required(),
+  url: Joi.custom((value) => {
+    if (!isValidUrl(value)) {
+      throw new Error('invalid URL');
+    }
+    return value;
+  }).required(),
+});
 export const registerSeller =
   (sellerService: SellerService) => (request: Request, response: Response) => {
-    const {
-      name: sellerName,
-      url: sellerUrl,
-      password: sellerPwd,
-    } = request.body;
-
-    if (!sellerName || !sellerUrl || !sellerPwd || !isValidUrl(sellerUrl)) {
+    const { error, value } = registerSellerValidator.validate(request.body);
+    if (error) {
       response
         .status(StatusCodes.BAD_REQUEST)
         .send({ message: 'missing name, password or url' });
-    } else if (sellerService.isAuthorized(sellerName, sellerPwd)) {
+      return;
+    }
+
+    const { name: sellerName, url: sellerUrl, password: sellerPwd } = value;
+    if (sellerService.isAuthorized(sellerName, sellerPwd)) {
       sellerService.register(sellerUrl, sellerName, sellerPwd);
       response.status(StatusCodes.OK).end();
     } else {
