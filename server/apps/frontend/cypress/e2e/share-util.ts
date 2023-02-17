83dc178cf60e269e90a875caf368d952e3afeec7
Sophie <sophie.danneels@bentley.com>
2023-02-17 18:29:16 -0500

    docker -suite

--- server/apps/frontend/cypress/e2e/share-util.ts
+++ server/apps/frontend/cypress/e2e/share-util.ts
@@ -1,7 +1,9 @@
 import { Given } from '@badeball/cypress-cucumber-preprocessor';
 
+export const baseUrl = 'http://localhost:3000/';
+
 Given(/^I'm on the website$/, () => {
   cy.get('html').invoke('css', 'height', 'initial');
   cy.get('body').invoke('css', 'height', 'initial');
-  cy.visit('http://localhost:3000/');
+  cy.visit(baseUrl);
 });
